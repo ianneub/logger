0014168ec0184c2730ebb55baad3aa008f875c92
Ian Neubert <ian@ianneubert.com>
2014-05-15 12:15:32 -0700

    Added initial logger library.

---
+++ logger.go
@@ -0,0 +1,17 @@
+package logger
+
+import "os"
+import "log"
+
+var errlog = log.New(os.Stderr, "", log.LstdFlags | log.Lmicroseconds)
+var stdlog = log.New(os.Stdout, "", log.LstdFlags | log.Lmicroseconds)
+
+// Fatal will log a message to Stderr and exit with 1
+func Fatal(format string, v ...interface{}) {
+  errlog.Fatalf(format, v...)
+}
+
+// Info will log a message to Stdout
+func Info(v ...interface{}) {
+  stdlog.Println(v...)
+}
