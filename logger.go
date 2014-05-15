a3dc9e9820c9414871ac54b35da6c7779b232f9b
Ian Neubert <ian@ianneubert.com>
2014-05-15 12:30:07 -0700

    Added DEBUG and Debug calls

--- logger.go
+++ logger.go
@@ -5,6 +5,18 @@ import "log"
 
 var errlog = log.New(os.Stderr, "", log.LstdFlags | log.Lmicroseconds)
 var stdlog = log.New(os.Stdout, "", log.LstdFlags | log.Lmicroseconds)
+var DEBUG = false
+
+// SetDebug will set the DEBUG variable
+func SetDebug(debug bool) {
+  DEBUG = debug
+}
+
+// IfDebug will return DEBUG
+func IfDebug() (debug bool) {
+  debug = DEBUG
+  return
+}
 
 // Fatal will log a message to Stderr and exit with 1
 func Fatal(format string, v ...interface{}) {
@@ -19,4 +31,9 @@ func Error(format string, v ...interface{}) {
 // Info will log a message to Stdout
 func Info(v ...interface{}) {
   stdlog.Println(v...)
+}
+
+// Debug will log a message to Stdout if DEBUG is enabled
+func Debug(v ...interface{}) {
+  if IfDebug() { stdlog.Println(v...) }
 }
